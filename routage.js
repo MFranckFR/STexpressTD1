54b8d379217df0bd9bee5def38c567c81c451e70
MFranckFR <76939469+MFranckFR@users.noreply.github.com>
2021-02-01 14:19:43 +0100

    Liste de personnes dans un template EJS

--- routage.js
+++ routage.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const personne = require('./routes/personne');
 
-app.use('/personne', personne); 
 // acces à cette reoute via localhost:8080/personne
 
+//EJS: Embedded Javascript
+app.set('engine_view', 'ejs');
+
 app.get('/', (req, resp) =>{
-    resp.send('Salut les gens !');
-})
+    let respText = '<p>Connectez vous <a href="/personne/toto">ICI</></p>';
+    resp.send(respText);
+});
+
+app.use(`/personne`, personne);
 
 app.listen(8080);
--- routes/personne.js
+++ routes/personne.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, resp) =>{
+    // const username = req.params.username ? req.params.username : 'Inconnu';   
+    const username = "Eren Jägger";
+    const personnes = [
+        {id:1, name:'John', surname:"Doe", age:22},
+        {id:2, name:'John', surname:"Wick", age:42},
+        {id:3, name:'John', surname:"Kennedy", age:132},
+        {id:4, name:'John', surname:"Jean", age:84}
+    ];
+    resp.render('index.ejs', {username:username, personnes:personnes});
+})
+
+router.get('/:id', (req, resp)=>{
+    resp.send(`Module Personne`);
+    })
+    // curl -i localhost:8080/personne/
+    
+router.get('/search', (req, resp)=>{
+    resp.send(`Recherche personne`);
+})
+
 router.post('/add', (req, resp)=>{
     resp.send(`Ajout de la personne`)
 })
 // curl -i -H "Content-Type: application/json" -X POST -d "{nom:'John Doe'}" localhost:8080/personne/add
-
-
-router.get('/', (req, resp)=>{
-    resp.send(`Module Personne`);
-})
-// curl -i localhost:8080/personne/
-
-router.get('/search', (req, resp)=>{
-    resp.send(`Recherche personne`);
-})
 
 router.put('edit', (req, resp)=>{
     req.send(`maj de la personne`);
@@ -25,5 +36,6 @@ router.delete('/delete', (req, resp)=>{
     resp.send('Suppression personne');
 })
 // curl -i -H "Content-Type: application/json" -X DELETE -d "{nom:'John Doe'}" localhost:8080/personne/delete
+
 
 module.exports = router;
