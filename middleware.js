2e66eadaa0e631fdc9981fd07502004880ea4fab
MFranckFR <76939469+MFranckFR@users.noreply.github.com>
2021-02-01 10:09:43 +0100

    2 middlewares: ordre mw1 puis mw2

--- middleware.js
+++ middleware.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
-const middleware = (req, resp, next) =>{
-    console.log(`middleware: ${req.url}`);
+const middleware1 = (req, resp, next) =>{
+    console.log(`middleware1: ${req.url}`);
     next();
 }
 
-app.use(middleware);
+const middleware2 = (req, resp, next) =>{
+    console.log(`middleware2: ${req.url}`);
+    //next();
+}
 
-app.get('/', (req, resp) =>{
+//app.use(middleware1);
+
+app.get('/', (req, resp, next) =>{
     console.log(`requete reçue`);
     resp.send('Salut les gens !');
-})
+    next();
+},  middleware1, // utilisé en 1er après "requete reçue"
+    middleware2) // utilisé en 2nd
 
 
 app.listen(8080, ()=>console.log(`Express en attente`));
